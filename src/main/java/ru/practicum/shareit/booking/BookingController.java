a835a5bf86f2dff914afa09888a917bb5de921da
karppow <tesserackt@yandex.ru>
2025-06-19 15:56:38 +0300

    Произвел каркас приложения, реализовал модели данных, создал DTO-объекты и мапперы, разработал контроллеры.

--- src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -1,12 +1,65 @@
 package ru.practicum.shareit.booking;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.booking.dto.BookingDto;
+import ru.practicum.shareit.booking.dto.BookingResponseDto;
+import ru.practicum.shareit.booking.service.BookingService;
 
-/**
- * TODO Sprint add-bookings.
- */
+import java.util.List;
+
+@Slf4j
 @RestController
-@RequestMapping(path = "/bookings")
+@RequestMapping("/bookings")
+@RequiredArgsConstructor
+@Validated
 public class BookingController {
+
+    private final BookingService bookingService;
+
+    @PostMapping
+    public BookingResponseDto createBooking(@RequestBody @Valid BookingDto bookingDto,
+                                            @Positive @RequestHeader("X-Sharer-User-Id") Long userId) {
+        log.info("POST /bookings by userId {}", userId);
+        return bookingService.createBooking(bookingDto, userId);
+    }
+
+
+    @PatchMapping("/{bookingId}")
+    public BookingResponseDto approveBooking(@Positive @PathVariable Long bookingId,
+                                             @Positive @RequestHeader("X-Sharer-User-Id") Long ownerId,
+                                             @RequestParam boolean approved) {
+        log.info("PATCH /bookings/{}?approved={} by ownerId {}", bookingId, approved, ownerId);
+        return bookingService.approveBooking(bookingId, ownerId, approved);
+    }
+
+
+    @GetMapping("/{bookingId}")
+    public BookingResponseDto getBookingById(@Positive @PathVariable Long bookingId,
+                                             @Positive @RequestHeader("X-Sharer-User-Id") Long userId) {
+        log.info("GET /bookings/{} by userId {}", bookingId, userId);
+        return bookingService.getBookingById(bookingId, userId);
+    }
+
+    @GetMapping
+    public List<BookingResponseDto> getBookingsByBooker(@Positive @RequestHeader("X-Sharer-User-Id") Long userId,
+                                                        @RequestParam String state,
+                                                        @RequestParam(defaultValue = "0") int from,
+                                                        @RequestParam(defaultValue = "10") int size) {
+        log.info("GET /bookings by bookerId {}, state={}, from={}, size={}", userId, state, from, size);
+        return bookingService.getBookingsByBooker(userId, state, from, size);
+    }
+
+    @GetMapping("/owner")
+    public List<BookingResponseDto> getBookingsByOwner(@Positive @RequestHeader("X-Sharer-User-Id") Long ownerId,
+                                                       @RequestParam(defaultValue = "ALL") String state,
+                                                       @RequestParam(defaultValue = "0") int from,
+                                                       @RequestParam(defaultValue = "10") int size) {
+        log.info("GET /bookings/ownerId by ownerId {}, state={}, from={}, size={}", ownerId, state, from, size);
+        return bookingService.getBookingsByOwner(ownerId, state, from, size);
+    }
 }
--- src/main/java/ru/practicum/shareit/request/ItemRequestController.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequestController.java
@@ -1,12 +1,44 @@
 package ru.practicum.shareit.request;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.request.dto.ItemRequestDto;
+import ru.practicum.shareit.request.dto.ItemRequestResponseDto;
+import ru.practicum.shareit.request.service.ItemRequestService;
 
-/**
- * TODO Sprint add-item-requests.
- */
+import java.util.List;
+
+
+@Slf4j
 @RestController
-@RequestMapping(path = "/requests")
+@RequiredArgsConstructor
+@RequestMapping("/requests")
+@Validated
 public class ItemRequestController {
+
+    private final ItemRequestService requestService;
+
+    @PostMapping
+    public ItemRequestResponseDto createRequest(@Positive @RequestHeader("X-Sharer-User-Id") Long userId,
+                                                @Valid @RequestBody final ItemRequestDto dto) {
+        log.info("POST /requests by userId {}", userId);
+        return requestService.create(userId, dto);
+    }
+
+
+    @GetMapping
+    public List<ItemRequestResponseDto> getOwnRequests(@Positive @RequestHeader("X-Sharer-User-Id") Long userId) {
+        log.info("GET /requests (own) by userId {}", userId);
+        return requestService.getOwnRequests(userId);
+    }
+
+    @GetMapping("/all")
+    public List<ItemRequestResponseDto> getAllRequests(@Positive @RequestHeader("X-Sharer-User-Id") Long userId) {
+        log.info("GET /requests/all by userId {}", userId);
+        return requestService.getAllRequests(userId);
+    }
 }
